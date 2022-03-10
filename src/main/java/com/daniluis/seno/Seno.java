ead52257f675f02b877085aa5c33f9a427e6adc3
Luis M. Blanco <lblaseg246@g.educaand.es>
2022-03-10 09:36:03 +0100

    cambios test

---
+++ src/main/java/com/daniluis/seno/Seno.java
@@ -0,0 +1,38 @@
+package com.daniluis.seno;
+
+
+import java.lang.Math;
+
+public class Seno {
+    
+
+
+
+   private int a;
+
+
+    Seno(int a){
+
+     
+     this.a=a;
+
+    }
+     
+
+
+    public static  int seno(int numero) {
+       
+       
+       return java.lang.Math.sin(int)(numero);
+     
+    }
+
+
+
+
+
+
+
+
+
+}
